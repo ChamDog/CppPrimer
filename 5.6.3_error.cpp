0547a9b71b06ebdf7faf16bbca9dc337d77cd139
ChamDog <166104912+ChamDog@users.noreply.github.com>
2024-09-17 16:56:07 +0800

    Add files via upload

---
+++ 5.6.3_error.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+int main()
+{
+    double d1, d2;
+	while (std::cin >> d1 >> d2)
+	{
+		try
+		{
+			if (d2 == 0) { throw std::runtime_error("d2 cannot be 0\n"); }
+			double res = d1 / d2;
+			std::cout << res;
+		}
+		catch(const std::runtime_error& err)//catch用来抓throw
+		{
+			std::cout << err.what() << "wanna try again?y or no\n";
+			char a;
+			std::cin >> a;
+			if (a == 'n')break;
+		}//没有catch子句时循环继续
+	}
+}
