4074634be2088fbf79b83be5e5004c46494064b0
ChamDog <166104912+ChamDog@users.noreply.github.com>
2024-09-17 17:38:22 +0800

    Add files via upload

---
+++ 8.6.1_io.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iostream>
+#include <string>
+
+std::istream& fun(std::istream& is)
+{
+    while (!is.eof())//ctrl Z + Enter
+    {
+        std::string s;
+        is >> s;
+        std::cout << s;
+    }
+    is.clear();
+    return is;
+}
+
+
+int main()
+{
+    fun(std::cin);
+}
