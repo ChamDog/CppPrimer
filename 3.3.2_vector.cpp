e64286aad5c417f0a1700f17631559c3aeea15ce
ChamDog <166104912+ChamDog@users.noreply.github.com>
2024-09-17 15:48:54 +0800

    Add files via upload

---
+++ 3.3.2_vector.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    /*int i;
+    std::vector<int>IntVector;
+    std::cout << "Input int:";
+    while (std::cin >> i) { IntVector.push_back(i); }*/
+
+    std::string s;
+    std::vector<std::string>StrVector;
+    std::cout << "Input string(EOF to back):";
+    while ((std::cin >> s) && s!="EOF") { StrVector.push_back(s); }
+
+    //仍然要分开来......
+}
---
+++ 3.3.3_vector.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    //3.17
+    std::vector<std::string>StrVector;
+    std::string s;
+    std::cout << "input words(EOF to back):";
+    while ((std::cin >> s)&&s!="EOF") 
+    {
+        StrVector.push_back(s); 
+    }
+    for (auto& s : StrVector)
+    {
+        for (auto& c : s)
+        {
+            c = ((c > 'A') && (c < 'Z')) ? c : c + ('A' - 'a');
+        }
+    }
+    for (auto& s : StrVector)
+    {
+        std::cout << s << '\t';
+    }
+    std::cout << std::endl;
+
+    ////3.18 以下代码将在运行时出错
+    //std::vector<int>iv;
+    //iv[0] = 42;
+
+    //3.19
+    std::vector<int>v1{ 42,42,42,42,42,42,42,42,42,42 };
+    std::vector<int>v2(10, 42);
+
+    //3.20
+    std::vector<int>vi{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int>::size_type pl = 0, pr = vi.size()-1;//别忘了减一
+    while (pl <= pr)
+    {
+        std::cout << vi[pl] + vi[pr] << " ";
+        pl++, pr--;
+    }
+    if (pl == pr)
+    {
+        std::cout << vi[pl] + vi[pr] << " ";
+    }
+}
