9d3b9d71448403e58a3f6d448b5e47426e3a49fd
ChamDog <166104912+ChamDog@users.noreply.github.com>
2024-09-17 14:51:33 +0800

    Add files via upload

---
+++ 3.2.2_string.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+//如果要识别空格呢
+
+int main()
+{
+    //3.2 从标准输入中一次读入一行
+    std::string line_3_2;
+    std::cout << "3.2 input a line:";
+    if (std::getline(std::cin, line_3_2))
+    {
+        std::cout << line_3_2 << "\t";
+    }
+    std::cout << std::endl << std::endl;
+
+    //3.2 从标准输入中一次读取一个
+    std::string word_3_3;
+    std::cout << "3.2 input some words(EOF to back):";
+    while ((std::cin >> word_3_3) && word_3_3 != "EOF")
+    {
+        std::cout << word_3_3 << "\t";
+    }
+    std::cout << std::endl << std::endl;
+
+    //3.4 比较相等与等长
+    std::string str1_3_4, str2_3_4;
+    std::cout << "3.4 input two string:";
+    std::cin >> str1_3_4 >> str2_3_4;
+    if (str1_3_4 == str2_3_4) { std::cout << "equal" << std::endl; }
+    else { std::cout << (str1_3_4 > str2_3_4 ? str1_3_4 : str2_3_4) << std::endl; }
+    if (str1_3_4.size() == str2_3_4.size()) { std::cout << "same size" << std::endl; }
+    else { std::cout << (str1_3_4.size() > str2_3_4.size() ? str1_3_4 : str2_3_4) << std::endl; }
+    std::cout << std::endl;
+
+    //3.5拼字符
+    std::string str,str_3_5;
+    std::cout << "3.5 input some string(EOF to back):";
+    while ((std::cin >> str_3_5) && str_3_5 != "EOF")
+    {
+        str = str + str_3_5 + " ";
+    }
+    std::cout << str << std::endl;
+}
+
---
+++ 3.2.3_string.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+int main()
+{
+    //3.6 3.7
+    std::string test_3_6("TestString");
+    for (auto& i : test_3_6) { i = 'X'; }
+    std::cout << test_3_6<<std::endl;
+    for (char& i : test_3_6) { i = 'Y'; }
+    std::cout << test_3_6<<std::endl;
+
+    //3.8
+    std::string::size_type cnt = test_3_6.size();
+    for (int i = 0; i < cnt; i++) { test_3_6[i] = 'Z'; }
+    std::cout << test_3_6 << std::endl;
+    int i = 0; while (i < cnt) { test_3_6[i] = 'A'; i++; }
+    std::cout << test_3_6 << std::endl;
+
+    //3.9
+    std::string s;
+    std::cout << s[0] << std::endl;
+
+    //3.10
+    std::string sen, temp;
+    std::cout << "input a sentence(no tap):"; std::cin >> sen;
+    for (auto c : sen)
+    {
+        if (!ispunct(c)) { temp += c; }
+    }
+    sen = temp;
+    std::cout << sen;
+
+    //3.11
+    //const std::string cs("Keep out!");
+    //for (auto& i : cs) { i++; }
+}
